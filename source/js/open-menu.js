e91fdee1daf0235f1977e0057c5676d87f58a433
chester425 <chester425@mail.ru>
2021-05-12 09:30:12 +0300

    декоротивные элементы index.html

---
+++ source/js/open-menu.js
@@ -0,0 +1,15 @@
+var navMain = document.querySelector('.main-menu');
+var navToggle = document.querySelector('.page-header__toggle');
+
+navMain.classList.remove('main-menu--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-menu--closed')) {
+    navMain.classList.remove('main-menu--closed');
+    navMain.classList.add('main-menu--opened');
+
+  } else {
+    navMain.classList.add('main-menu--closed');
+    navMain.classList.remove('main-menu--opened');
+  }
+});
